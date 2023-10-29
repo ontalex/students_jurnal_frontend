5692a555931d05eb9c581e3e0cd72a84d2f4b3a8
Aleksey Borisov <loja.bobaev2000@yandex.ru>
2023-10-29 21:14:44 +0300

    add: подсказка какая страница выбрана

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,43 +2,66 @@ import { NavLink } from "react-router-dom";
 
 import "./Menu.css";
 
-import {ReactComponent as Home_icon} from "../../pic/home_icon.svg";
-import {ReactComponent as Notebook_icon} from "../../pic/notebook_icon.svg";
-import {ReactComponent as Calendar_icon} from "../../pic/calendar_icon.svg";
-import {ReactComponent as Profile_icon} from "../../pic/profile_icon.svg";
-import {ReactComponent as Export_icon} from "../../pic/export_icon.svg";
+import { ReactComponent as Home_icon } from "../../pic/home_icon.svg";
+import { ReactComponent as Notebook_icon } from "../../pic/notebook_icon.svg";
+import { ReactComponent as Calendar_icon } from "../../pic/calendar_icon.svg";
+import { ReactComponent as Profile_icon } from "../../pic/profile_icon.svg";
+import { ReactComponent as Export_icon } from "../../pic/export_icon.svg";
 
-import React from 'react'
+import React from "react";
 
 export const Menu = () => {
-  return (
-    <nav className="menu">
+    return (
+        <nav className="menu">
+            <NavLink
+                to={``}
+                className={({ isPending }) =>
+                    isPending ? "menu__link menu__link--active" : "menu__link"
+                }
+            >
+                <Home_icon className="menu__icon" />
+                <span className="menu__name">Главная</span>
+            </NavLink>
 
-      <NavLink to={`../admin`} className="menu__link">
-        <Home_icon className="menu__icon" />
-        <span className="menu__name">Главная</span>
-      </NavLink>
+            <NavLink
+                to={`schedule`}
+                className={({ isActive }) =>
+                    isActive ? "menu__link menu__link--active" : "menu__link"
+                }
+            >
+                <Notebook_icon className="menu__icon" />
+                <span className="menu__name">Пары</span>
+            </NavLink>
 
-      <NavLink to={`schedule`} className="menu__link">
-        <Notebook_icon className="menu__icon" />
-        <span className="menu__name">Пары</span>
-      </NavLink>
+            <NavLink
+                to={`logbook`}
+                className={({ isActive }) =>
+                    isActive ? "menu__link menu__link--active" : "menu__link"
+                }
+            >
+                <Calendar_icon className="menu__icon" />
+                <span className="menu__name">Учёт</span>
+            </NavLink>
 
-      <NavLink to={`logbook`} className="menu__link">
-        <Calendar_icon className="menu__icon" />
-        <span className="menu__name">Учёт</span>
-      </NavLink>
-      
-      <NavLink to={`profile`} className="menu__link">
-        <Profile_icon className="menu__icon" />
-        <span className="menu__name">Профиль</span>
-      </NavLink>
+            <NavLink
+                to={`profile`}
+                className={({ isActive }) =>
+                    isActive ? "menu__link menu__link--active" : "menu__link"
+                }
+            >
+                <Profile_icon className="menu__icon" />
+                <span className="menu__name">Профиль</span>
+            </NavLink>
 
-      <NavLink to={`export`} className="menu__link">
-        <Export_icon className="menu__icon" />
-        <span className="menu__name">Отчёт</span>
-      </NavLink>
-
-    </nav>
-  )
-}
+            <NavLink
+                to={`export`}
+                className={({ isActive }) =>
+                    isActive ? "menu__link menu__link--active" : "menu__link"
+                }
+            >
+                <Export_icon className="menu__icon" />
+                <span className="menu__name">Отчёт</span>
+            </NavLink>
+        </nav>
+    );
+};
